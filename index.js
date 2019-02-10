0084d45d7c898f24d539895391b020fe002253af
Alejandro Cena <alejandro.cena@gmail.com>
2019-02-10 01:32:45 -0300

    Fagged result as boolean

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let redis;
 
 const DEFAULT = {
   DB: 0,
+  PORT: 6379,
   PREFIX:'flagger::',
   TTL:60,
   KEY_BUILDER: payload => {
@@ -12,21 +13,21 @@ const DEFAULT = {
   },
 };
 
-const setup = ({host,port,db=DEFAULT.DB,password='',ttl=DEFAULT.TTL,key_builder=DEFAULT.KEY_BUILDER,key_prefix=DEFAULT.PREFIX}) => {
+const setup = ({host,port=DEFAULT.PORT,db=DEFAULT.DB,password='',ttl=DEFAULT.TTL,key_builder=DEFAULT.KEY_BUILDER,key_prefix=DEFAULT.PREFIX}) => {
   DEFAULT.TTL = ttl;
   DEFAULT.KEY_BUILDER = key_builder;
   redis = new Redis({host,port,db,password});
   DEFAULT.PREFIX = key_prefix;
 };
 
-const flag = (payload,{ttl=DEFAULT.TTL}) => {
+const flag = (payload,ttl=DEFAULT.TTL) => {
   if(!redis) throw 'Connection was not Stablished';
   redis.set(DEFAULT.KEY_BUILDER(payload), 1, 'EX', ttl);
 };
 
 const flagged = async (payload) => {
   if(!redis) throw 'Connection was not Stablished';
-  return await redis.exists(DEFAULT.KEY_BUILDER(payload));
+  return await redis.exists(DEFAULT.KEY_BUILDER(payload)).then(res => res === 1);
 };
 
 module.exports = {
